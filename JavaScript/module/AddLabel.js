cc589c5322ff3a5636260e5a2f2b6abca4d3ad82
evelyn401 <evelynlin401@gmail.com>
2025-10-31 22:21:13 +0800

    feat: 新增AddLabel函數以創建並設定卡片內容的div元素

---
+++ JavaScript/module/AddLabel.js
@@ -0,0 +1,7 @@
+export function AddLabel() {
+    // 無迴圈
+    // 1. 在 .card 之內加入 div,cardcontent
+    let div = document.createElement('div');
+    // 2. 設定上述元素的 class 屬性為 cardcontent
+    div.className = 'cardcontent';
+}
