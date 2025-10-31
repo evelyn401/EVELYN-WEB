981763cb736ca9eea293a5e77020946dd372e305
evelyn401 <evelynlin401@gmail.com>
2025-10-31 22:58:29 +0800

    feat: 為技能區塊新增SkillsCard ID以支援標籤功能

--- JavaScript/module/AddLabel.js
+++ JavaScript/module/AddLabel.js
@@ -1,7 +1,10 @@
 export function AddLabel() {
+    // 主架構 宣告變數
+    let card = document.getElementById('SkillsCard');
     // 無迴圈
     // 1. 在 .card 之內加入 div,cardcontent
     let div = document.createElement('div');
     // 2. 設定上述元素的 class 屬性為 cardcontent
     div.className = 'cardcontent';
+    // 3. 
 }
