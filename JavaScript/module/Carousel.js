1f09391d80feb2787cd0be43d500771dd60d520d
evelyn401 <evelynlin401@gmail.com>
2025-11-26 13:08:38 +0800

    feat: 將所有相關的PhoneBanners更名為Banners，更新樣式及HTML結構

--- JavaScript/module/Carousel.js
+++ JavaScript/module/Carousel.js
@@ -2,7 +2,7 @@ export function Carousel(){
     // 圖片索引（幾張圖片）
     let index = 0;
     // 取得圖片元素 宣告「常數」變數 ; 常數指的是「值不能更改」
-    const images = document.querySelectorAll('#PhoneBanners .Images');
+    const images = document.querySelectorAll('#Banners .Images');
     // 計算圖片總數（圖片張數）
     const totalImages = images.length;
 
