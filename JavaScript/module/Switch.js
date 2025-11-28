3e98d11d2bdcb8ca8e134c9d85a00fc3d0fa2327
evelyn401 <evelynlin401@gmail.com>
2025-11-28 13:52:38 +0800

    feat: 修正SwitchTab函數名稱大小寫，新增導覽列ID以改善結構化標記

--- JavaScript/module/Switch.js
+++ JavaScript/module/Switch.js
@@ -1,3 +1,12 @@
-export function switchTab() {
-    
+export function SwitchTab() {
+    // 宣告變數 讀取選單按鈕 開啟選單按鈕
+    let MenuBtn = document.getElementById("MenuBtn");
+    // 宣告變數 讀取導覽列
+    let NavBox = document.getElementById("NavBox");
+
+    // 點擊選單按鈕時
+    MenuBtn.addEventListener("click", function() {
+        // 切換導覽列的顯示狀態
+        NavBox.classList.toggle("active");
+    });
 }
